663c9a129dac61eff6a97e363e84074e8bbbedeb
Shane Craven Palomo <palomoshanecraven@gmail.com>
2025-03-05 22:46:17 +0800

    Delete Python Program Batch 1 directory

--- Python Program Batch 1/Prog 09.py
+++
@@ -1,9 +0,0 @@
-#CHECKS THE EVEN NUMBER FROM 0 - 100
-
-nums = []
-
-for num in range(101):
-    if num % 2 == 0:
-        nums.append(num)
-
-print(f"Even numbers are {nums}.")
--- Python Program Batch 1/Prog 10.py
+++
@@ -1,9 +0,0 @@
-#CHECK IF THE NUMBERS ENDS WITH 0
-
-nums = []  
-
-for num in range(101):
-    if not str(num).endswith("0"): 
-        nums.append(num)
-
-print(nums)
